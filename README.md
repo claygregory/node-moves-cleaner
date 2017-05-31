640be2e528c5c41800adde03a2dc67c92b4a99ff
Clay Gregory <r@claygregory.com>
2017-05-31 18:28:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+
+# Moves App Segment Cleaner
+
+Having worked with data from the [Moves App](https://www.moves-app.com) —both from the API as well as manual JSON exports — I've noticed a few
+recurring oddities I attempt to correct with this utility. Namely:
+
+ * Long stays at a single location (in excess of 24 hours) tend to get truncated, forming a time gap
+ * Occasionally a single stay or single move will get chopped into multiple segments
+ * Other time gaps inexplicably appear between segments, absent an 'off' segment
+ * Not specifically a problem, but multple consecutive movements (e.g. walking → transport → walking) are merged as activities under a single 'move' segment. I prefer these separated into separate segments to simplify analysis.
+
+## Usage
+
+ For most applications, just call the single `apply` method on an array of segments.
+
+ ```javascript
+const MovesCleaner = require('@claygregory/moves-cleaner');
+
+const movesCleaner = new MovesCleaner();
+const normalizedSegments = movesCleaner.apply([
+  { type: 'move', activities: […], … },
+  { type: 'place', activities: […], … },
+  …
+]);
+ ```
+
+### Additional Methods
+
+Individual normalization steps can be applied individually, which are typically applied in succession via `apply`. These include:
+
+#### Close Gaps
+
+Collapses the gap between two segments so long as no `off` segments are logged and the distance between the shoulder segments is within
+a given threshold.
+
+```javascript
+movesCleaner.close_gaps([…]);
+```
+
+#### Filter Off Segments
+
+Removes segments with a `type` value of `off`. The gaps in time remain, only the segments are removed.
+
+```javascript
+movesCleaner.filter_off_segments([…]);
+```
+
+#### Flatten Move Segments
+
+Bubbles the individual activities of `move` segments up as standalone move segments.
+
+```javascript
+movesCleaner.flatten_move_segments([…]);
+```
+
+#### Merge Move Segments
+
+Merges consecutive move segments of same type into a single segment. Track points are merged and start/end time, duration, and distance are corrected.
+
+```javascript
+movesCleaner.merge_move_segments([…]);
+```
+
+#### Merge Place Segments
+
+Merges consecutive place segments with same place ID into a single segment. Start/end times are corrected.
+
+```javascript
+movesCleaner.merge_place_segments([…]);
+```
+
+#### Sort
+
+Orders segments according to time. Many of the above methods assume time-ordered segments are provided.
+
+```javascript
+movesCleaner.sort_segments([…]);
+```
+
+### Options
+
+ Currently only one configuration option is available: `near_threshold_m` is used in gap detection to determine when the end of one segment is close enough to the beginning of next. Gaps are only closed between if endpoints are within threshold. The default is 100 meters.
+
+ ```javascript
+const MovesCleaner = require('@claygregory/moves-cleaner');
+
+const movesCleaner = new MovesCleaner({
+  near_threshold_m: 250
+});
+ ```
+
+## License
+
+See the included [LICENSE](LICENSE.md) for rights and limitations under the terms of the MIT license.
---
+++ index.js
@@ -0,0 +1,203 @@
+'use strict';
+
+const distance = require('fast-haversine');
+const moment = require('moment');
+const _ = require('lodash');
+
+const default_options = {
+  near_threshold_m: 100
+};
+
+class MovesCleaner {
+
+  constructor(options) {
+    this.options = _.defaults({}, options, default_options);
+  }
+
+  close_gaps(segments) {
+
+    return MovesCleaner._map_segments(segments, (current_segment, previous_segment, next_segment) => {
+
+      if (current_segment.type !== 'place')
+        return current_segment;
+
+      if (previous_segment) {
+        const previous_end = _.last(MovesCleaner._locations_of_segment(previous_segment));
+        const current_start = _.first(MovesCleaner._locations_of_segment(current_segment));
+
+        if (MovesCleaner._is_location_near(current_start, previous_end, this.options.near_threshold_m))
+          current_segment = _.merge({}, current_segment, { startTime: previous_segment.endTime });
+      }
+
+      if (next_segment) {
+        const current_end= _.last(MovesCleaner._locations_of_segment(current_segment));
+        const next_start = _.first(MovesCleaner._locations_of_segment(next_segment));
+
+        if (MovesCleaner._is_location_near(current_end, next_start, this.options.near_threshold_m))
+          current_segment = _.merge({}, current_segment, { endTime: next_segment.startTime });
+      }
+
+      return current_segment;
+    });
+  }
+
+  filter_off_segments(segments) {
+    return _.filter(segments, segment => segment.type !== 'off');
+  }
+
+  flatten_move_segments(segments) {
+    return _.chain(segments)
+      .flatMap(segment => {
+
+        if (segment.type === 'move' && segment.activities) {
+          return _.map(segment.activities, activity => {
+            return _.defaults(activity, {
+              type: 'move',
+              startTime: segment.startTime,
+              endTime: segment.endTime
+            });
+          });
+        } else {
+          return segment;
+        }
+
+      })
+      .filter()
+      .uniqBy(segment => `${segment.type} ${moment(segment.startTime).unix()} ${moment(segment.endTime).unix()}`)
+      .value();
+  }
+
+  merge_move_segments(segments) {
+    return this._merge_segments(segments, MovesCleaner._is_same_move, (a, b) => {
+
+      const merged_trackPoints = _(a.trackPoints || [])
+        .concat(b.trackPoints || [])
+        .value();
+
+      return _.merge({}, a, {
+        endTime: b.endTime,
+        duration: moment.duration(b.endTime, a.startTime).asSeconds(),
+        distance: MovesCleaner._path_distance(merged_trackPoints),
+        trackPoints: merged_trackPoints
+      });
+    });
+  }
+
+  merge_place_segments(segments) {
+    return this._merge_segments(segments, MovesCleaner._is_same_place, (a, b) => {
+
+      const merged_activities = _(a.activities || [])
+        .concat(b.activities || [])
+        .uniqWith(MovesCleaner._is_same_segment)
+        .value();
+
+      return _.merge({}, a, {
+        endTime: b.endTime,
+        activities: merged_activities
+      });
+    });
+  }
+
+  apply(segments) {
+
+    const process_flow = _.flow([
+      this.flatten_move_segments,
+      this.sort_segments,
+      this.merge_move_segments,
+      this.merge_place_segments,
+      this.close_gaps,
+      this.filter_off_segments,
+    ]);
+
+    return _.bind(process_flow, this)(segments);
+  }
+
+  sort_segments(segments) {
+    return _.sortBy(segments, segment => moment(segment.startTime).unix());
+  }
+
+  static _is_location_near(a, b, threshold_m) {
+    if (a == null || b == null)
+      return false;
+
+    return  distance(a, b) < threshold_m;
+  }
+
+  static _is_same_move(a, b) {
+    if( a.type === 'move' && b.type === 'move') {
+      return a.activity === b.activity;
+    } else {
+      return false;
+    }
+  }
+
+  static _is_same_place(a, b) {
+    if( a.type === 'place' && b.type === 'place') {
+      return a.place.id === b.place.id;
+    } else {
+      return false;
+    }
+  }
+
+  static _is_same_segment(a, b) {
+    return a.type === b.type &&
+      moment(a.startTime).isSame(b.startTime) && moment(a.endTime).isSame(b.endTime);
+  }
+
+  static _map_segments(segments, iteree) {
+    let prev = null;
+    return _.map(segments, (segment, index) => {
+      let next = null;
+      if (index + 1 < segments.length - 1)
+        next = segments[index + 1];
+
+      return prev = iteree(segment, prev, next);
+    });
+  }
+
+  static _locations_of_segment(segment) {
+    if (segment.type === 'place')
+      return [segment.place.location];
+    else
+      return _.map(segment.trackPoints, s => _.omit(s, 'time'));
+  }
+
+  static _path_distance(points) {
+    const sum = _.reduce(points, (sum, point, i) => {
+      if (i + 1 < points.length - 1) {
+        return sum + distance(point, points[i + 1]);
+      } else {
+        return sum;
+      }
+    }, 0);
+
+    return Math.round(sum);
+  }
+
+  _merge_segments(segments, comparator, merger) {
+
+    const merged_segments = [];
+
+    let end = 1;
+    for (let start = 0; start < segments.length; start = end) {
+
+      const start_segment = segments[start];
+      for (end = start + 1; end < segments.length; end++) {
+        const end_segment = segments[end];
+        if (!comparator(start_segment, end_segment))
+          break;
+      }
+
+      let merged_segment = start_segment;
+      for (let i = start + 1; i < end; i++) {
+        merged_segment = merger(merged_segment, segments[i]);
+      }
+      merged_segments.push(merged_segment);
+    }
+
+    return merged_segments;
+  }
+
+}
+
+module.exports = MovesCleaner;
